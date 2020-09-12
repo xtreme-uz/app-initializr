0278b314b8728c2333267a786179e7cebf279e29
xtreme-uz <rustambekov.mail.box@gmail.com>
2020-09-12 18:20:11 +0500

    feat: initial commit

---
+++ src/main/java/uz/xtreme/defaultstarter/DefaultStarterApplication.java
@@ -0,0 +1,13 @@
+package uz.xtreme.defaultstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/xtreme/defaultstarter/DefaultStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.xtreme.defaultstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
